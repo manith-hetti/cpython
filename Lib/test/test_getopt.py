7a807c90e877481f577c030b68aad15576069332
manith-hetti <manithhetti@gmail.com>
2026-07-18 14:00:14 +0100

    Add test coverage to check execution of GetoptError.__str__

--- Lib/test/test_getopt.py
+++ Lib/test/test_getopt.py
@@ -18,11 +18,17 @@ class GetoptTests(unittest.TestCase):
     def assertError(self, *args, **kwargs):
         self.assertRaises(getopt.GetoptError, *args, **kwargs)
 
+    def assertErrorMessage(self, *args, message, **kwargs):
+        self.assertRaisesRegex(getopt.GetoptError, message, *args, **kwargs)
+
     def test_short_has_arg(self):
         self.assertIs(getopt.short_has_arg('a', 'a:'), True)
         self.assertIs(getopt.short_has_arg('a', 'a'), False)
         self.assertEqual(getopt.short_has_arg('a', 'a::'), '?')
         self.assertError(getopt.short_has_arg, 'a', 'b')
+        self.assertErrorMessage(
+            getopt.short_has_arg, 'a', 'b', message='option -a not recognized'
+        )
 
     def test_long_has_args(self):
         has_arg, option = getopt.long_has_args('abc', ['abc='])
@@ -50,8 +56,17 @@ class GetoptTests(unittest.TestCase):
         self.assertEqual(option, 'abcd')
 
         self.assertError(getopt.long_has_args, 'abc', ['def'])
+        self.assertErrorMessage(
+            getopt.long_has_args, 'abc', ['def'], message='option --abc not recognized'
+        )
         self.assertError(getopt.long_has_args, 'abc', [])
         self.assertError(getopt.long_has_args, 'abc', ['abcd','abcde'])
+        self.assertErrorMessage(
+            getopt.long_has_args,
+            'abc',
+            ['abcd', 'abcde'],
+            message='option --abc not a unique prefix; possible options: abcd, abcde',
+        )
 
     def test_do_shorts(self):
         opts, args = getopt.do_shorts([], 'a', 'a', [])
@@ -83,7 +98,13 @@ class GetoptTests(unittest.TestCase):
         self.assertEqual(args, [])
 
         self.assertError(getopt.do_shorts, [], 'a1', 'a', [])
+        self.assertErrorMessage(
+            getopt.do_shorts, [], 'a1', 'a', [], message='option -1 not recognized'
+        )
         self.assertError(getopt.do_shorts, [], 'a', 'a:', [])
+        self.assertErrorMessage(
+            getopt.do_shorts, [], 'a', 'a:', [], message='option -a requires argument'
+        )
 
     def test_do_longs(self):
         opts, args = getopt.do_longs([], 'abc', ['abc'], [])
@@ -126,6 +147,14 @@ class GetoptTests(unittest.TestCase):
         self.assertEqual(args, [])
 
         self.assertError(getopt.do_longs, [], 'abc=1', ['abc'], [])
+        self.assertErrorMessage(
+            getopt.do_longs,
+            [],
+            'abc=1',
+            ['abc'],
+            [],
+            message='option --abc must not have an argument',
+        )
         self.assertError(getopt.do_longs, [], 'abc', ['abc='], [])
 
     def test_getopt(self):
@@ -150,6 +179,13 @@ class GetoptTests(unittest.TestCase):
         self.assertEqual(args, ['arg1', 'arg2'])
 
         self.assertError(getopt.getopt, cmdline, 'a:b', ['alpha', 'beta'])
+        self.assertErrorMessage(
+            getopt.getopt,
+            cmdline,
+            'a:b',
+            ['alpha', 'beta'],
+            message='option --alpha must not have an argument',
+        )
 
     def test_gnu_getopt(self):
         # Test handling of GNU style scanning mode.
